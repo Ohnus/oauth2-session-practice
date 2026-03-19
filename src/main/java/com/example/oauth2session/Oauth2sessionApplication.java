6a2501da816bdc9045f48ffadd651e6880393dc6
SUNHO <sh525293@gmail.com>
2026-03-19 12:37:06 +0900

    OAuth2 Session First Commit

---
+++ src/main/java/com/example/oauth2session/Oauth2sessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2sessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2sessionApplication.class, args);
+	}
+
+}
